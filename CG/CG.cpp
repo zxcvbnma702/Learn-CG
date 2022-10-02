adc2d3e338e0f5b1919f0f812914c37a36b461bc
SunShibo <2990452909@qq.com>
2022-10-02 23:09:03 +0800

    Draw a point

--- CG/CG.cpp
+++ CG/CG.cpp
@@ -2,13 +2,63 @@
 #include <GLFW\glfw3.h>
 #include <iostream>
 
+#define numVAOs 1
+
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+
+GLuint createShaderProgram() {
+    const char *vshaderSource =
+        "#version 430 \n"
+        "void main(void) \n"
+        "{ gl_Position = vec4(0.0, 0.0, 0.0, 1.0); }";
+
+    const char *fshaderSource = 
+        "#version 430 \n"
+        "out vec4 color; \n"
+        "void main(void) \n"
+        "{ color = vec4(0.0, 0.0, 1.0, 1.0); }";
+
+    // Create a vertex shader
+    GLuint vShader = glCreateShader(GL_VERTEX_SHADER);
+    // Create a fragment shader
+    GLuint fShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    // Load GLSL code from string into the empty shader object
+    glShaderSource(vShader, 1, &vshaderSource, NULL);
+    glShaderSource(fShader, 1, &fshaderSource, NULL);
+
+    // Compile each shader
+    glCompileShader(vShader);
+    glCompileShader(fShader);
+
+    // Create an openGl program object
+    GLuint vfProgram = glCreateProgram();
+    // Attach the shader to openGl program object
+    glAttachShader(vfProgram, vShader);
+    glAttachShader(vfProgram, fShader);
+    glLinkProgram(vfProgram);
+
+    return vfProgram;
+}
+
 using namespace std;
 
-void init(GLFWwindow* window) { }
+void init(GLFWwindow* window) {
+    renderingProgram = createShaderProgram();
+    /*!
+        Even if the application doesn't use any buffers at all, 
+        OpenGL still needs at least one created VAO when using shaders, 
+        so these two lines are used to create the VAO required by OpenGL.
+    */
+    glGenVertexArrays(numVAOs, vao);
+    glBindVertexArray(vao[0]);
+}
 
 void display(GLFWwindow* window, double currentTime) {
-    glClearColor(1.0, 0.0, 0.0, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT);
+    //Load shaders into hardware
+    glUseProgram(renderingProgram);
+    glDrawArrays(GL_POINTS, 0, 1);
 }
 
 int main(void) {
