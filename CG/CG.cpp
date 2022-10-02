a83d6929591082a32744cafeaa8f0d3504cddd32
SunShibo <2990452909@qq.com>
2022-10-02 22:36:06 +0800

    Create a openGl window

--- CG/CG.cpp
+++ CG/CG.cpp
@@ -13,11 +13,19 @@ void display(GLFWwindow* window, double currentTime) {
 
 int main(void) {
     if (!glfwInit()) { exit(EXIT_FAILURE); }
+
+    //Specify the openGl version number
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    //Create the openGl context 
     GLFWwindow* window = glfwCreateWindow(600, 600, "Chapter2 - program1", NULL, NULL);
+    //Link the openGl context and glfwWindow
     glfwMakeContextCurrent(window);
+
     if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE); }
+
+    //Turn on vertical sync
     glfwSwapInterval(1);
 
     init(window);
